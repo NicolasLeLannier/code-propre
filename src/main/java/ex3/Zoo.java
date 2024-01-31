d2e3856548b7cad6f6bf5b7b8cac0d976ab93806
LE LANNIER <lelannier.nicolas@gmail.com>
2024-01-31 12:05:10 +0100

    corriger exo 3

--- src/main/java/ex3/Zoo.java
+++ src/main/java/ex3/Zoo.java
@@ -1,33 +1,41 @@
 package ex3;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/** Représente le concept de Zoo
+ * @author Nicolas LE LANNIER
+ */
 public class Zoo {
 
-	/** types de l'animal */
-	private List<TypeAnimal> types;
-	/** noms de l'animal */
-	private List<Animal> noms;
-	/** comportements alimentaire de l'animal */
-	private List<ComportementAlimentaire> comportements;
+	/** nom du zoo */
+	private String nom;
+	/** animaux */
+	private List<Animal> animaux = new ArrayList<>();
 	
+	/** Constructeur
+	 * @param nom
+	 */
+	public Zoo(String nom) {
+		super();
+		this.nom = nom;
+	}
+
 	/** Method
 	 * @param nomAnimal : nom de l'animal
 	 * @param typeAnimal : type de l'animal
 	 * @param comportement : comportement alimentaire de l'animal
 	 */
-	public void addAnimal(Animal nomAnimal, TypeAnimal typeAnimal, ComportementAlimentaire comportement){
-		types.add(typeAnimal);
-		noms.add(nomAnimal);
-		comportements.add(comportement);
+	public void addAnimal(Animal animal){
+		animaux.add(animal);
 	}
 	
 	/** Method
 	 * afficher liste animaux
 	 */
 	public void afficherListeAnimaux(){
-		for (int i=0; i<types.size(); i++) {
-			System.out.println(noms.get(i).getNom()+" "+types.get(i)+" "+comportements.get(i));
+		for (Animal animal : animaux) {
+			System.out.println(animal);
 		}
 	}
 	
@@ -35,7 +43,21 @@ public class Zoo {
 	 * @return
 	 */
 	public int taille() {
-		return types.size();
+		return animaux.size();
+	}
+
+	/** Getter
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/** Setter
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
 	}
 
 }
--- src/main/java/ex3/ZooApplication.java
+++ src/main/java/ex3/ZooApplication.java
@@ -3,16 +3,19 @@ package ex3;
 public class ZooApplication {
 
 	public static void main(String[] args) {
-		Zoo zoo = new Zoo();
+		Zoo zoo = new Zoo("Beauval");
 		
-		zoo.addAnimal(new Animal("Gazelle"), TypeAnimal.MAMMIFERE, ComportementAlimentaire.HERBIVORE);
-		zoo.addAnimal(new Animal("Zèbre"), TypeAnimal.MAMMIFERE, ComportementAlimentaire.HERBIVORE);
-		zoo.addAnimal(new Animal("Lion"), TypeAnimal.MAMMIFERE, ComportementAlimentaire.HERBIVORE);
-		zoo.addAnimal(new Animal("Panthère"), TypeAnimal.MAMMIFERE, ComportementAlimentaire.CARNIVORE);
-		zoo.addAnimal(new Animal("Requin blanc"), TypeAnimal.POISSON, ComportementAlimentaire.HERBIVORE);
-		zoo.addAnimal(new Animal("Truite dorée"), TypeAnimal.POISSON, ComportementAlimentaire.HERBIVORE);
-		zoo.addAnimal(new Animal("Boa constrictor"), TypeAnimal.SERPENT, ComportementAlimentaire.CARNIVORE);
-		zoo.addAnimal(new Animal("Python"), TypeAnimal.SERPENT, ComportementAlimentaire.CARNIVORE);
+		zoo.addAnimal(new Animal("Gazelle", EspeceAnimal.MAMMIFERE, ComportementAlimentaire.HERBIVORE));
+		zoo.addAnimal(new Animal("Zèbre", EspeceAnimal.MAMMIFERE, ComportementAlimentaire.HERBIVORE));
+		zoo.addAnimal(new Animal("Lion", EspeceAnimal.MAMMIFERE, ComportementAlimentaire.HERBIVORE));
+		zoo.addAnimal(new Animal("Panthère", EspeceAnimal.MAMMIFERE, ComportementAlimentaire.CARNIVORE));
+		zoo.addAnimal(new Animal("Requin blanc", EspeceAnimal.POISSON, ComportementAlimentaire.HERBIVORE));
+		zoo.addAnimal(new Animal("Truite dorée", EspeceAnimal.POISSON, ComportementAlimentaire.HERBIVORE));
+		zoo.addAnimal(new Animal("Boa constrictor", EspeceAnimal.REPTILE, ComportementAlimentaire.CARNIVORE));
+		zoo.addAnimal(new Animal("Python", EspeceAnimal.REPTILE, ComportementAlimentaire.CARNIVORE));
+		
+		zoo.afficherListeAnimaux();
+		System.err.println(zoo.taille());
 	}
 
 }
